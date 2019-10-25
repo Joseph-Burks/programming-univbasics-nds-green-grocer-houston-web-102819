2aaa971f83ec92fe0f9e37e1028bc483256327a8
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-25 20:18:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,9 @@ def apply_coupons(cart, coupons)
     count_is_large_enough = item_in_cart && item_has_coupon[:count] >= single_coupon[:num]
     if item_in_cart && count_is_large_enough
       apply_the_coupon(item_has_coupon, coupon, cart)
-      
+    end
+    i += 1
+    
 end
 
 def apply_clearance(cart)
